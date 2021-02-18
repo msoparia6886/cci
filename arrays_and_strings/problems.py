6afe56b2c3bd198a9c24ccf84fd5a1591f1f0059
vena <vena@netapp.com>
2021-02-17 17:02:30 -0700

    Add isUnique problem solution - Anil

--- arrays_and_strings/problems.py
+++ arrays_and_strings/problems.py
@@ -13,3 +13,25 @@ cannot use additional data structures?"""
     greater = [i for i in arr if i > pivot]
     return is_unique(lesser) and is_unique(greater)
 
+
+def isUnique(array):
+    """Anil find if array has duplicates"""
+
+    less = []
+    equal = []
+    greater = []
+
+    if len(array) > 1:
+        pivot = array[0]
+        for x in array:
+            if x < pivot:
+                less.append(x)
+            elif x == pivot:
+                equal.append(x)
+            elif x > pivot:
+                greater.append(x)
+        if len(equal) > 1:
+            return False
+        return isUnique(less) and isUnique(greater)
+    else:
+        return True
