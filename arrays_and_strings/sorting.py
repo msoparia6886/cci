c317fbe7ac513a2de609432cf4324b32a704b457
Maitrey <Maitrey.Soparia@netapp.com>
2021-02-19 15:21:02 -0700

    More problems

--- arrays_and_strings/sorting.py
+++ arrays_and_strings/sorting.py
@@ -5,5 +5,5 @@ def quicksort(arr):
         return []
     pivot = arr.pop(-1)
     lesser = [i for i in arr if i < pivot]
-    greater = [i for i in arr if i > pivot]
+    greater = [i for i in arr if i >= pivot]
     return quicksort(lesser) + [pivot] + quicksort(greater)
